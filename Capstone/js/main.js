273978dd57a1835acc12b7ecde9bf493aa531788
Macy <macylau94@gmail.com>
2022-04-28 12:18:46 -0600

    working on populating recipes

--- Capstone/js/main.js
+++ Capstone/js/main.js
@@ -1,4 +1,4 @@
-const options = {
+const recipes = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
@@ -6,8 +6,31 @@ const options = {
 	}
 };
 
-fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=salad', options)
+fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&q=pho', recipes)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+	console.log(recipes)
+
+const loadedRecipes = []
+
+async function loadRecipes(from = 0, size = 10) {
+	const recipeData = await getAPIData(
+	  `https://tasty.p.rapidapi.com/recipes/list?from=${from}&size=${size}&q=pho`,
+	)
+	for (const nameAndUrl of recipeData.results) {
+	  const recipe = await getAPIData(nameAndUrl.url)
+	  const simplifiedRecipes = {
+		name: recipe.name,
+		num_servings: recipe.num_servings,
+		ingredients: recipe.sections[0].components,
+		instructions: recipe.instructions,
+	  }
+	  console.log(recipe.sections[0].components)
+	  loadedRecipes.push(simplifiedRecipes)
+	  populateRecipes(simplifiedRecipes)
+	}
+  }
+
 
