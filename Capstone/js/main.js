7adc24fdb03b7e09c80a366f9b3491a2ec5d3b02
Macy <macylau94@gmail.com>
2022-04-29 08:05:05 -0600

    upgraded stypes

--- Capstone/js/main.js
+++ Capstone/js/main.js
@@ -1,5 +1,5 @@
 async function showResults(searchWiki) {
-  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchWiki}`;
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=15&srsearch=${searchWiki}`;
   await fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
